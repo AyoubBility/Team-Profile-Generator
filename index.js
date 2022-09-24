b2b265061360bd4bcc5d86a89b5dadd32f0f96f9
Ayoub <biliad8786@gmail.com>
2022-09-24 16:17:28 -0400

    base questions and libs are done

---
+++ index.js
@@ -0,0 +1,37 @@
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const teamMembers = [];
+const idArray = [];
+
+function createTeam() {
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "memberChoice",
+            message: "Which type of team member would you like to add?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "I don't want to add any more team members"
+            ]
+        }
+    ]).then(userChoice => {
+        switch (userChoice.memberChoice) {
+            case "Engineer":
+                console.log('You added an Engineer.')
+                break;
+            case "Intern":
+                console.log('You added an Intern.')
+                break;
+                
+        }
+    });
+}
+createTeam();
